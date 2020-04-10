4f00386c64bf636020fcaa4fa84deffe027b7a4b
AliAbdulle <alidhobey@gmail.com>
2020-04-10 14:14:16 -0500

    error needs to fixed

---
+++ src/Components/Card/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from './Cards'
+
+function App () {
+    return (
+        <div>
+            <Cards cardColor="red" />
+            <Cards cardColor="yellow" />
+            <Cards cardColor="green" />
+        </div>
+
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
-import App from './Components/Calculator/App'
+import App from './Components/Card/App'
 
 ReactDOM.render(
 <App />, document.getElementById('root'));
