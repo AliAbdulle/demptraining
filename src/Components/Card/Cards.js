3ff9449115a50c712e0915071aab4e19192f2fbb
AliAbdulle <alidhobey@gmail.com>
2020-04-10 15:22:26 -0500

    added defualtProps and propsType

--- src/Components/Card/Cards.js
+++ src/Components/Card/Cards.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
+import propsTypes from 'prop-types'
 
-class Cards extends Component {
-    static defaultProps ={
-            cardColor: "Blue",
-            height: 100,
-            width: 100
+function Cards (props) {
+    const styles = {
+        backgroundColor: props.cardColor,
+        height: props.height,
+        width: props.width
     }
-    render() {
-        const styles = {
-            backgroundColor: this.props.cardColor,
-            height: 100,
-            width: 100
-        }
+
     return(
         <div style={styles}></div>
     )
-    }
+
+}
+
+Cards.propsTypes ={
+    backgroundColor: propsTypes.string.isRequired,
+    height: propsTypes.number.isRequired,
+    width: propsTypes.number
+
+}
+
+
+Cards.defaultProps = {
+    cardColor: 'blue',
+    height: 100,
+    width:100
 }
 
 export default Cards
