bbc9384642cddb4956eae28659938dbee06fb4e6
AliAbdulle <alidhobey@gmail.com>
2020-04-09 19:58:32 -0500

    Button plus and minus

--- src/Components/Calculator/NewJSFeatures.js
+++ src/Components/Calculator/NewJSFeatures.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 
 
 class NewJSFeatures extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count : 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.state.increment}>+</button>
+                <button onClick={this.state.decrement}>-</button>
+            </div>
+        )
+    }
 
 }
 
