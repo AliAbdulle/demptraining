8ae12df6d819ad9f638458e2553a8bfad78d2760
AliAbdulle <alidhobey@gmail.com>
2020-04-10 19:34:32 -0500

    Saved the files

--- src/Components/Callouts/App.js
+++ src/Components/Callouts/App.js
@@ -8,8 +8,14 @@ function App(){
         <main>
             <h1>Welcome!</h1>
             <infoCallout />
-            <ImagCallout />
-            <EmailCallout />
+            <ImagCallout 
+                img={"https://picsum.photos/id/102/4320/3240"}
+                caption="Just look at those sparkling raspberries!"
+            />
+            <EmailCallout
+                 header="Give us your email. We definitely won't sell it to anyone." 
+                 btnText="Sign me up!" 
+             />
         </main>
     )
 }
