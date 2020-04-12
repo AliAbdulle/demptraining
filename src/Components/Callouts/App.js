a3f6d8ea40edbadbe76f9ef5ed952e16c7b76b95
AliAbdulle <alidhobey@gmail.com>
2020-04-11 21:27:16 -0500

    Saved the change

--- src/Components/Callouts/App.js
+++ src/Components/Callouts/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import InfoCallout from './InfoCallout'
 import EmailCallout from './EmailCallout'
-import ImagCallout from './ImageCallout'
+import ImageCallout from './ImageCallout'
 
 function App(){
     return (
@@ -14,10 +14,12 @@ function App(){
             />
              <p>This is probably the best site you've ever come across. I'm glad you're here 
                  to witness the magnificence of this website right now.</p>
-            <ImagCallout 
+            <ImageCallout 
                 img={"https://picsum.photos/id/102/4320/3240"}
                 caption="Just look at those sparkling raspberries!"
             />
+            <p>Here's some more unforgettable content. Lorem ipsum something or other.</p>
+
             <EmailCallout
                  header="Give us your email. We definitely won't sell it to anyone." 
                  btnText="Sign me up!" 
