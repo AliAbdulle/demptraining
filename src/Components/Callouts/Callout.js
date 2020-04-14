eca3b11cef35f6371c5db8cf3d1bbf019e0e3ba1
AliAbdulle <alidhobey@gmail.com>
2020-04-13 20:46:21 -0500

    Saved all the file

--- src/Components/Callouts/Callout.js
+++ src/Components/Callouts/Callout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Callout(props) {
     return (
-
+//children props
         <div className="callout">
             {props.children}
         </div>
--- src/Components/Card/App.js
+++ src/Components/Card/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from './Cards'
-
+//three different cards
 function App () {
     return (
         <div>
