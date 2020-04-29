178d07fb7d180ecb942c981c6c5b7c56dcc9f74c
AliAbdulle <alidhobey@gmail.com>
2020-04-29 15:14:05 -0500

    new folder and file has been created

---
+++ src/Components/Context/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+class Button extends Component {
+    render() {
+        return ( 
+            <Button className="light-Them">Switch Them</Button>
+        );
+    }
+}
+ 
+export default Button;
