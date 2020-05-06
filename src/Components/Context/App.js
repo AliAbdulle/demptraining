6adb9668c18d85b57273169fabdc0d78bdee8e94
AliAbdulle <alidhobey@gmail.com>
2020-05-06 18:40:41 -0500

    Added new buttom with different theme

--- src/Components/Context/App.js
+++ src/Components/Context/App.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 import Header from "./Header"
 import Button from "./Button"
+import ThemeContext from "./ThemeContext";
+
 
 function App() {
     return (
         <div>
             <Header />
+            <ThemeContext.Consumer>
+                {theme => (
+            <Button  theme={theme}/>
+                )}
+            </ThemeContext.Consumer>
             <Button />
         </div>
     )
