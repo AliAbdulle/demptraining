fc6f04338ee5d49fc5d85821e903a400969d7626
AliAbdulle <alidhobey@gmail.com>
2020-05-06 15:59:37 -0500

    Changed all class components to functional component

--- src/Components/Context/Header.js
+++ src/Components/Context/Header.js
@@ -1,17 +1,16 @@
-import React, {Component} from "react"
-import ThemeContext from "./ThemeContext"
+import React from "react";
+import ThemeContext from "./ThemeContext";
 
-
-class Header extends Component {
-    static contextType = ThemeContext
-    render() {
-        const theme = this.context
-        return (
-            <header className={`${theme}-theme`}>
-                <h2>{theme === "light" ? "Light" : "Dark"}Light Theme</h2>
-            </header>
-        )
-    }
+function Header() {
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <header className={`${theme}-theme`}>
+          <h2>{theme === "light" ? "Light" : "Dark"}Light Theme</h2>
+        </header>
+      )}
+    </ThemeContext.Consumer>
+  );
 }
 
-export default Header
+export default Header;
