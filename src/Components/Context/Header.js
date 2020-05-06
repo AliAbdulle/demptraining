5f3291af885c26c61dfd52f109b01815d062a195
AliAbdulle <alidhobey@gmail.com>
2020-05-06 15:36:09 -0500

    contect logic style

--- src/Components/Context/Header.js
+++ src/Components/Context/Header.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react"
+import ThemeContext from "./ThemeContext"
+
 
 class Header extends Component {
+    static contextType = ThemeContext
     render() {
+        const theme = this.context
         return (
-            <header className="light-theme">
-                <h2>Light Theme</h2>
+            <header className={`${theme}-theme`}>
+                <h2>{theme === "light" ? "Light" : "Dark"}Light Theme</h2>
             </header>
         )
     }
