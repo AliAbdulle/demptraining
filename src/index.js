af01a801944434c18e0c6a42cc087f985f268768
AliAbdulle <alidhobey@gmail.com>
2020-05-09 21:33:22 -0500

    loaded all thefiles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./Components/Context/App";
 import {ThemeContextProvider} from "./Components/Context/ThemeContext";
 
-
+//Added all file display the page
 ReactDOM.render(
   <ThemeContextProvider value={"dark"}>
     <App />
