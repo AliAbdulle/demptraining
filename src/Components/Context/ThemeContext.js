4accb931245e559182c61227f1e7247818b1f598
AliAbdulle <alidhobey@gmail.com>
2020-05-09 21:10:42 -0500

    Identify the state

--- src/Components/Context/ThemeContext.js
+++ src/Components/Context/ThemeContext.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 const {Provider, Consumer} = React.createContext()
 
 class ThemeContextProvider extends Component {
+    //state display the color
     state = {
         theme: 'light'
     }
