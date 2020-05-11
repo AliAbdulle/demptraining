64a169f877b285334a8d82d8f482985b425fe33e
AliAbdulle <alidhobey@gmail.com>
2020-05-10 21:42:20 -0500

    Hooks state initiated the file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/Context/App";
-import {ThemeContextProvider} from "./Components/Context/ThemeContext";
+import App from "./Components/HookState/App";
+//import {ThemeContextProvider} from "./Components/Context/ThemeContext";
+
 
 //Added all file display the page
 ReactDOM.render(
-  <ThemeContextProvider value={"dark"}>
-    <App />
-  </ThemeContextProvider>,
+    <App />,
   document.getElementById("root")
 );
 
