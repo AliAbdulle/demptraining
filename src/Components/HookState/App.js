bca65c5ed4b3eb4a4c41fc31265e7195767fb210
AliAbdulle <alidhobey@gmail.com>
2020-05-10 21:49:23 -0500

    State array

--- src/Components/HookState/App.js
+++ src/Components/HookState/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
 function App() {
-    const value = useState("Yes")
+    const [value] = useState("Yes")
     return (
         <div>
-            <h1>Is state important to know? {value[0]}</h1>
+            <h1>Is state important to know? {value}</h1>
         </div>
     )
 }
