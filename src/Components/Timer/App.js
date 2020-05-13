655da0631a203d14fb1d386e41c5e2be82afb07f
AliAbdulle <alidhobey@gmail.com>
2020-05-13 09:53:25 -0500

    add font style

--- src/Components/Timer/App.js
+++ src/Components/Timer/App.js
@@ -10,7 +10,10 @@ function App(){
     return (
         <div>
             <h1> How fast do you type?</h1>
-            <textarea onChange={handleChange}/>
+            <textarea 
+                onChange={handleChange}
+                value={text}
+            />
             <h4>Time remaining: ???</h4>
             <button>Start</button>
             <h1>Word count: ???</h1>
