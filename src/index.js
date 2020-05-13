8c876adb098e68a0b6fe1aecb283beb31b8ecfd5
AliAbdulle <alidhobey@gmail.com>
2020-05-12 22:25:23 -0500

    handlechange the text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/HookState/App";
+import App from "./Components/Timer/App";
 //import {ThemeContextProvider} from "./Components/Context/ThemeContext";
 
 
