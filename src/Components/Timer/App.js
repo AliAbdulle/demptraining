e9d3a1fbe092d52a2f245004c78864066baf5b9a
AliAbdulle <alidhobey@gmail.com>
2020-05-12 22:11:25 -0500

    display the speeding game

--- src/Components/Timer/App.js
+++ src/Components/Timer/App.js
@@ -4,6 +4,10 @@ function App(){
     return (
         <div>
             <h1> How fast do you type?</h1>
+            <textarea />
+            <h4>Time remaining: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
         </div>
     )
 }
